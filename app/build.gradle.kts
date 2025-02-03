78f65d24631e100f3959115efde3c432d489807e
Vandy Rizky Septiawan <vandyrizky33@gmail.com>
2025-02-04 07:37:55 +0800

    coil update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation("io.coil-kt.coil3:coil-compose:3.0.4")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
 
     val nav_version = "2.8.4"
 
